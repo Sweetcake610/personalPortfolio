b7af808f12d302d76204e66098251b66d2083c1c
Aisha Coles <coles_aisha@yahoo.com>
2019-03-04 12:56:06 -0800

    added js file to project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// JavaScript File. Adding animation to main page and adding project section
+
+var circles = document.getElementsByClassName('circle');
+
+
+function circleAnimation(circles) {
+	var circlesArray = [...circles];
+}
+circles.addEventListener('load', circleAnimation);
