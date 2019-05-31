996d4474313b6b536d80433f02b419e16c78e974
Aisha Coles <coles_aisha@yahoo.com>
2019-05-31 15:24:59 -0700

    adding style changes to fit js project section

--- js/app.js
+++ js/app.js
@@ -21,4 +21,24 @@ for(let j = 0; j < circlesArray.length; j++) {
 }
 
 //Adding projects to project section 
+const projectSection = document.getElementById('project');
 
+class Project {
+	constructor(title, description, year, source) {
+		this._title = title,
+		this._description = description,
+		this._year = year,
+		this._source = source
+	}
+	get projectInfo () {
+		let projectDiv = document.createElement('div');
+		projectDiv.setAttribute('class', 'project-info');
+		projectDiv.innerHTML = '<h3>Project Title:' + this._title + '</h3>' +
+								'<p>Description:' + this._description + '<br>Year Completed:' +
+								this._year + '<br>Source:' + this._source + '</p>';
+		projectSection.appendChild(projectDiv);
+	}
+	
+}
+let projectOne = new Project('C&L Catering Distribution', 'Create full live website', '2019', 'http://...');
+projectOne.projectInfo;
